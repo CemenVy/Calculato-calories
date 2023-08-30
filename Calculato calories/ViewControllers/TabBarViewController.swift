22ae037dbd4f0c254ee0359d569760f2809052d7
CemenVy <semen.vydrin0108@gmail.com>
2023-08-30 19:13:14 +0400

    Added new classes

--- Calculato calories/ViewControllers/TabBarViewController.swift
+++ Calculato calories/ViewControllers/TabBarViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TabBarViewController.swift
 //  Calculato calories
 //
 //  Created by Семен Выдрин on 30.08.2023.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
-
