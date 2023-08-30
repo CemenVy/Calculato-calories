238b0000b4c2dd4adb000e1629bb569b6c557074
CemenVy <semen.vydrin0108@gmail.com>
2023-08-30 15:01:05 +0400

    Initial Commit

---
+++ Calculato calories/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculato calories
+//
+//  Created by Семен Выдрин on 30.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
