b9ac2f3ed2af1239987dfce052f4cb2e47c1b826
CemenVy <semen.vydrin0108@gmail.com>
2023-09-18 20:02:49 +0400

    Work with logic and Model

--- Calculato calories/Models/Calculator.swift
+++ Calculato calories/Models/Calculator.swift
@@ -9,11 +9,8 @@ import Foundation
 
 struct Calculator {
     let person: Person
-    let bMR: BMR
     
-    static func getCalculator() -> Double {
-        
-        let person = Person.getPerson()
+    func getCalculator() -> Double {
         
         var genderCoefficient = 0.0
         var weightCoefficient = 0.0
@@ -47,15 +44,15 @@ struct Person {
     var growth: Double
     var weight: Double
     
-    static func getPerson() -> Person {
-        Person(gender: .male,
-               goal: .gainWeight,
-               levelActivity: .low,
-               age: 27,
-               growth: 180,
-               weight: 70
-        )
+    init(gender: Gender, goal: Goal, levelActivity: Activity, age: Double, growth: Double, weight: Double) {
+        self.gender = gender
+        self.goal = goal
+        self.levelActivity = levelActivity
+        self.age = age
+        self.growth = growth
+        self.weight = weight
     }
+
 }
 
 enum BMR: Double {
@@ -67,7 +64,7 @@ enum BMR: Double {
     case femaleGrowthCoefficient = 1.8
     case femaleAgeCoefficient = 4.7
     case femaleBaseCoefficient = 655
-
+    
 }
 
 enum Gender {
@@ -75,16 +72,40 @@ enum Gender {
     case female
 }
 
-enum Activity: Double {
+enum Activity: Double, CaseIterable {
     case low = 1.2
     case lowPlus = 1.375
     case medium = 1.55
     case high = 1.725
+    
+    var definition: String {
+        switch self {
+        case .low:
+            return "Не тренируюсь"
+        case .lowPlus:
+            return "Редко тренируюсь"
+        case .medium:
+            return "Регулярно тренируюсь"
+        case .high:
+            return "Профессиональный спортсмен"
+        }
+    }
 }
 
-enum Goal: Double {
+enum Goal: Double, CaseIterable {
     case keepInShape = 0.8
     case reduceWeight = 1.0
     case gainWeight = 1.2
+    
+    var definition: String {
+        switch self {
+        case .keepInShape:
+            return "Уменьшить вес"
+        case .reduceWeight:
+            return "Сохранить текущий вес"
+        case .gainWeight:
+            return "Набрать вес"
+        }
+    }
 }
 
--- Calculato calories/ViewControllers/CalculateViewController.swift
+++ Calculato calories/ViewControllers/CalculateViewController.swift
@@ -7,12 +7,129 @@
 
 import UIKit
 
-final class CalculateViewController: UIViewController {
+enum CurrentMenu {
+    case activityMenu
+    case goalMenu
+}
 
+final class CalculateViewController: UIViewController {
+    
+    @IBOutlet var maleButton: UIButton!
+    @IBOutlet var femaleButton: UIButton!
+    
+    @IBOutlet var ageTextField: UITextField!
+    @IBOutlet var weightTextField: UITextField!
+    @IBOutlet var growthTextField: UITextField!
+    
+    @IBOutlet var levelActivityMenu: UIButton!
+    @IBOutlet var goalMenu: UIButton!
+    
+    private var person:Person!
+    
+    private let buttonOn: CGFloat = 1
+    private let buttonOf: CGFloat = 0.3
+    
+    private var currentMenu: CurrentMenu = .activityMenu
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        ageTextField.delegate = self
+        weightTextField.delegate = self
+        growthTextField.delegate = self
+        
+        levelActivityMenu.layer.cornerRadius = 10
+        goalMenu.layer.cornerRadius = 10
     }
+    
+    private func createMenu(for currentMenu: CurrentMenu) -> UIMenu? {
+        let menuTitle = "Выберите вариант"
+        var actions: [UIAction] = []
+        
+        switch currentMenu {
+        case .activityMenu:
+            actions = Activity.allCases.map { activity in
+                UIAction(title: activity.definition) { [weak self] _ in
+                    self?.levelActivityMenu.setTitle(activity.definition, for: .normal)
+                    self?.person?.levelActivity = activity
+                }
+            }
+        case .goalMenu:
+            actions = Goal.allCases.map { goal in
+                UIAction(title: goal.definition) { [weak self] _ in
+                    self?.goalMenu.setTitle(goal.definition, for: .normal)
+                    self?.person.goal = goal
+                }
+            }
+        }
+        
+        let menu = UIMenu(title: menuTitle, children: actions)
+        return menu
+    }
+    
+    private func showAlert(with title: String, andMessage message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert,animated: true)
+    }
+    
+    @IBAction func chooseGenderDidTapped(_ sender: UIButton) {
+        switch sender {
+        case maleButton:
+            person.gender = .male
+            femaleButton.alpha = buttonOf
+            maleButton.alpha = buttonOn
+        default:
+            person.gender = .female
+            maleButton.alpha = buttonOf
+            femaleButton.alpha = buttonOn
+        }
+    }
+    
+    @IBAction func chooseLevelActivityDidTapped(_ sender: UIButton) {
+        sender.menu = createMenu(for: .activityMenu)
+        
+    }
+    
+    @IBAction func chooseGoalDidTapped(_ sender: UIButton) {
+        sender.menu = createMenu(for: .goalMenu)
+    }
+    
+    @IBAction func calculateButtonDidTapped(_ sender: UIButton) {
+    }
+}
 
-
+extension CalculateViewController: UITextFieldDelegate {
+    func resignFirstResponder(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text  else  {
+            showAlert(with: "Внимание!", andMessage: "Пожалуйста введите корректное значение.")
+            return
+        }
+        guard let correctValue = Double(text), (0.0...250.0).contains(correctValue) else {
+            showAlert(with: "Не верный формат!",
+                      andMessage: "Пожалуйста в видите число которое соответствует вашим параметрам.",
+                      textField: textField
+            )
+            return
+        }
+        
+        switch textField {
+        case ageTextField:
+            person.age = correctValue
+        case weightTextField:
+            person.weight = correctValue
+        default:
+            person.growth = correctValue
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+    }
 }
 
