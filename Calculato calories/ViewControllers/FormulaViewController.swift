8bc3f2f740c899f0be751bdb307813022161974b
CemenVy <semen.vydrin0108@gmail.com>
2023-09-28 18:39:09 +0400

    Work with UI in storyboard

---
+++ Calculato calories/ViewControllers/FormulaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FormulaViewController.swift
+//  Calculato calories
+//
+//  Created by Семен Выдрин on 28.09.2023.
+//
+
+import UIKit
+
+class FormulaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
