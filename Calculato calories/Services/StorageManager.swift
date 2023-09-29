1d0c6541ecd263c56e7cbef7f3923c2f457506a5
CemenVy <semen.vydrin0108@gmail.com>
2023-09-29 15:56:07 +0400

    Work with logic in StorageManager

--- Calculato calories/Services/StorageManager.swift
+++ Calculato calories/Services/StorageManager.swift
@@ -10,10 +10,22 @@ import Foundation
 final class StorageManager {
     static let shared = StorageManager()
     
+    private let fileURL = URL.documentsDirectory.appending(path: "Persons").appendingPathExtension("plist")
+    
     private init() {}
     
-    func create() {
+    func fetchPersons() -> [Person] {
+        guard let data = try? Data(contentsOf: fileURL) else { return [] }
+        guard let persons = try? PropertyListDecoder().decode([Person].self, from: data) else { return [] }
+        return persons
+    }
+    
+    func save(person: Person) {
+        var persons = fetchPersons()
+        persons.append(person)
         
+        guard let data = try? PropertyListEncoder().encode(persons) else { return }
+        try? data.write(to: fileURL, options: .noFileProtection)
     }
     
     func read() {
@@ -24,7 +36,11 @@ final class StorageManager {
         
     }
     
-    func delete() {
+    func deletePerson(at index: Int) {
+        var persons = fetchPersons()
+        persons.remove(at: index)
         
+        guard let data = try? PropertyListEncoder().encode(persons) else { return }
+        try? data.write(to: fileURL, options: .noFileProtection)
     }
 }
